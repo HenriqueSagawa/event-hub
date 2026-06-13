cfd308a4de1a0aeab18ed46ddec9aaf7f492f2ea
HenriqueSagawa <henriquetutomusagawa@gmail.com>
2026-06-13 09:38:31 -0300

    feat: add Prisma configuration and schema files, update environment variable validation

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -8,6 +8,7 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(["development", "production", "test"])
     .default("development"),
+  DATABASE_URL: z.url(),
 });
 
 const _env = envSchema.safeParse(process.env);
